7d95e28cb95f74856a9c23daad030020b6708c6a
Brandon Gregory <bgthemanofsteel@gmail.com>
2019-02-13 14:01:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+$('#js-shopping-list-form').submit(function(event){
+    let item = $('#shopping-list-entry').val();
+    $('.shopping-list-entry').val('');
+    $('.shopping-list').append(
+        `<li>
+            <span class="shopping-item">${item}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+            </button>
+            </div>
+        </li>`);
+
+    alert("yum " + item + '!');
+    event.preventDefault();
+
+});
+
+
+$('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    $(this).closest("li").find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+$('.shopping-list').on('click', '.shopping-item-delete', function(event){
+    $(this).closest('li').remove();
+});
